a9885e07573553ff376c3866fb74eef4bb67b139
rahaf <rahaf@RAHAFSK>
2026-06-23 14:47:12 +0400

    5. View Passengers

--- FlighManagementSystem/Program.cs
+++ FlighManagementSystem/Program.cs
@@ -250,6 +250,26 @@ namespace FlightManagementSystem.Models
             Console.WriteLine("Flight created successfully.");
         }
 
+        // ==========================================================
+        // 5. View Passengers
+        // ==========================================================
+        static void ViewPassengers()
+        {
+            Console.Clear();
+            Console.WriteLine("===== All Passengers =====");
+
+            if (!context.Passengers.Any())
+            {
+                Console.WriteLine("No passengers found.");
+                return;
+            }
+
+            foreach (Passenger passenger in context.Passengers)
+            {
+                Console.WriteLine($"ID: {passenger.passengerId} | Name: {passenger.passengerName} | Phone: {passenger.passengerPhone} | Email: {passenger.passengerEmail} | Passport: {passenger.passportNumber}");
+            }
+        }
+
 
         // ==========================================================
         // Main Menu
