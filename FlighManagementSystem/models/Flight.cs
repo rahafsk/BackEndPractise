b58462e018de544053c9ef988123576c634af9d7
rahaf <rahaf@RAHAFSK>
2026-06-23 09:50:02 +0400

    Class Flight

--- FlighManagementSystem/models/Flight.cs
+++ FlighManagementSystem/models/Flight.cs
@@ -6,5 +6,17 @@ namespace FlighManagementSystem.models
 {
     internal class Flight
     {
+        public int flightId { get; set; }
+        public string flightNumber { get; set; }
+        public int pilotId { get; set; }
+        public int aircraftId { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public string departureDate { get; set; }
+        public string departureTime { get; set; }
+        public decimal ticketPrice { get; set; }
+        public int totalSeats { get; set; }
+        public int availableSeats { get; set; }
+        public string status { get; set; }
     }
 }
