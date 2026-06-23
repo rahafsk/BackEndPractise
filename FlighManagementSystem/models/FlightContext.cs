2549ef686bc35a1a57cadfbf9a81f918ab87cfe1
rahaf <rahaf@RAHAFSK>
2026-06-23 10:02:52 +0400

    class FlightContext

--- FlighManagementSystem/models/FlightContext.cs
+++ FlighManagementSystem/models/FlightContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FlighManagementSystem.Models;
+using FlightManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,10 @@ namespace FlighManagementSystem.models
 {
     internal class FlightContext
     {
-
+        public List<Passenger> Passengers { get; set; }
+        public List<Pilot> Pilots { get; set; }
+        public List<Aircraft> Aircrafts { get; set; }
+        public List<Flight> Flights { get; set; }
+        public List<Booking> Bookings { get; set; }
     }
 }
