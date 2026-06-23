d8edf90487c11f951e128f63c5b1d2fac2b0028b
rahaf <rahaf@RAHAFSK>
2026-06-23 09:28:12 +0400

    class Pilot

--- FlighManagementSystem/models/Passenger.cs
+++ FlighManagementSystem/models/Passenger.cs
@@ -7,7 +7,7 @@ namespace Task2_FlightManagementSystem.Models
     {
     public class Passenger
     {
-        public int passengerId { get; set; }
+            public int passengerId { get; set; }
             public string passengerName { get; set; }
             public string passengerPhone { get; set; }
             public string passengerEmail { get; set; }
--- FlighManagementSystem/models/Pilot.cs
+++ FlighManagementSystem/models/Pilot.cs
@@ -6,6 +6,11 @@ namespace Task2_FlightManagementSystem.Models
 {
     public class Pilot
     {
+        public int pilotId { get; set; }
+        public string pilotName { get; set; }
+        public string licenseNumber { get; set; }
+        public int experienceYears { get; set; }
+        public bool isAvailable { get; set; }
 
     }
 }
