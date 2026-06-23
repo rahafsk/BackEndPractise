fa069bbbe8d86d9457111f108da20884962ef19d
rahaf <rahaf@RAHAFSK>
2026-06-23 10:11:43 +0400

    Main storage

--- FlighManagementSystem/Program.cs
+++ FlighManagementSystem/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FlighManagementSystem.models;
+using FlighManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +9,14 @@ namespace FlightManagementSystem.Models
 {
     public class  Program
     {
-        
+        // Main storage for the whole flight system
+        public static FlightContext context = new FlightContext
+        {
+            Passengers = new List<Passenger>(),
+            Pilots = new List<Pilot>(),
+            Aircrafts = new List<Aircraft>(),
+            Flights = new List<Flight>(),
+            Bookings = new List<Booking>()
+        };
     }
 }
