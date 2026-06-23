8276d61903750804b7b569ce2de7b0cc3df5f640
rahaf <rahaf@RAHAFSK>
2026-06-23 09:24:06 +0400

    Class Passenger

---
+++ FlighManagementSystem/models/Passenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2_FlightManagementSystem.Models
+
+    {
+    public class Passenger
+    {
+        public int passengerId { get; set; }
+            public string passengerName { get; set; }
+            public string passengerPhone { get; set; }
+            public string passengerEmail { get; set; }
+            public string passportNumber { get; set; }
+        }
+    }
+
+
