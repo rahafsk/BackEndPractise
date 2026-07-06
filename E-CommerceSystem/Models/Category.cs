43ca41f6af1e512a21adbf6d8ae3722d6ceeaf24
rahaf <rahaf@RAHAFSK>
2026-07-07 01:09:25 +0400

    class Category

--- E-CommerceSystem/Models/Category.cs
+++ E-CommerceSystem/Models/Category.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Class1
+namespace ECommerceSystem.Models
 {
-	public Class1()
-	{
-	}
+    [Table("Categories")]
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int categoryId { get; set; } // system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string categoryName { get; set; } = string.Empty; // user input, unique
+
+        [MaxLength(500)]
+        public string? description { get; set; } // user input, optional
+
+        [MaxLength(300)]
+        public string? imageUrl { get; set; } // user input, optional
+
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+    }
 }
