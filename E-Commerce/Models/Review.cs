f0ccca1357103a44a06ff76ca998046780fe0ff7
rahaf <rahaf@RAHAFSK>
2026-07-07 23:04:03 +0400

    Class Review

--- E-Commerce/Models/Review.cs
+++ E-Commerce/Models/Review.cs
@@ -1,10 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace E_Commerce.Models
+namespace ECommerceSystem.Models
 {
-    internal class Review
+    [Table("Reviews")]
+    public class Review
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int reviewId { get; set; } // system generated
+
+        [Required]
+        [ForeignKey("U")]
+        public int userId { get; set; } // foreign key
+
+        [Required]
+        [ForeignKey("product")]
+        public int productId { get; set; } // foreign key
+
+        [Required]
+        [Range(1, 5)]
+        public int rating { get; set; } // user input
+
+        [MaxLength(1000)]
+        public string? comment { get; set; } // user input, optional
+
+        [Required]
+        public DateTime reviewDate { get; set; } // system generated
+
+        public virtual User U { get; set; } = null!;
+        public virtual Product product { get; set; } = null!;
     }
 }
