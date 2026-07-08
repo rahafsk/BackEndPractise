316b4902da12bcf0a0622e34c8eb9462ec97b7e2
rahaf <rahaf@RAHAFSK>
2026-07-08 13:27:31 +0400

    class ECommerceContext

--- E-Commerce/ECommerceContext.cs
+++ E-Commerce/ECommerceContext.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using ECommerceSystem.Models;
 
-namespace E_Commerce
+namespace ECommerce_System
 {
-    internal class ECommerceContext
+    internal class ECommerceContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        // Bridge table between Order and Product
+        public DbSet<OrderItem> OrderProducts { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+            @"Server=localhost\MSSQLSERVER02;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
     }
 }
